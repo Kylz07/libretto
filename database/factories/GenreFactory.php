40111876a1e081e835e1a91a217025695eb8c8b9
jspeleca09 <jspeleca09@scs.local>
2025-06-24 16:36:55 +0800

    Initial commit

---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Genre;
+use Illuminate\Database\Eloquent\Factories\Factory;
+class GenreFactory extends Factory
+{
+ protected $model = Genre::class;
+ public function definition()
+ {
+ return [
+ 'name' => ucfirst($this->faker->words(2, true)) ,
+ ];
+ }
+}
